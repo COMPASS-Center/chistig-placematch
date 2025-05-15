aaf80c97d6e112c2b455afdf5582cdee71bac84a
Tom Wolff <tom.wolff@northwestern.edu>
2025-05-15 09:59:21 -0400

    Added some missing pieces to the `utils` script, added missing `params.csv` file

--- 03-epimodel-parameter-calibration/step3_utils.R
+++ 03-epimodel-parameter-calibration/step3_utils.R
@@ -32,6 +32,7 @@
 # intervention_start <- yaml$intervention_start
 # intervention_end   <- yaml$intervention_end
 
+
 est_dir <- "./03-epimodel-parameter-calibration/data/intermediate/estimates/"
 diag_dir <- "./03-epimodel-parameter-calibration/data/intermediate/diagnostics/"
 calib_dir <- "./03-epimodel-parameter-calibration/data/intermediate/calibration/"
@@ -618,6 +619,43 @@ swf_configs_quest <- function(partition = partition,
 }
 
 ######
+
+# Custom `swf_configs` function for Quest made by Adrien
+swf_configs_quest <- function(partition = "short",
+                              account = "p32153",
+                              r_version = "4.3.0",
+                              conda_proj = NULL,
+                              mail_user = "tom.wolff@northwestern.edu") {
+
+  hpc_configs <- list()
+  hpc_configs[["default_sbatch_opts"]] <-  list(
+    "partition" = partition,
+    "account" = account,
+    "mail-type" = "FAIL"
+  )
+
+  if (!is.null(mail_user)) {
+    hpc_configs[["default_sbatch_opts"]][["mail-user"]] <- mail_user
+  }
+
+  hpc_configs[["renv_sbatch_opts"]] <- EpiModelHPC:::swf_renv_sbatch_opts()
+
+  hpc_configs[["r_loader"]] <- c(
+    "module purge all",
+    paste0("module load R/", r_version),
+    "module load git"
+  )
+
+  if (!is.null(conda_proj)) {
+    hpc_configs[["r_loader"]] <- c(hpc_configs[["r_loader"]], c(
+      "module load python-miniconda3",
+      paste0("source activate /projects/", conda_proj ,"/pythonenvs/env1")
+    ))
+  }
+
+  return(hpc_configs)
+}
+
 
 hpc_configs <- swf_configs_quest(
   partition = partition, # ASK ABOUT THIS
