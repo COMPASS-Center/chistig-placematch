ca0c7f63f5b061f4a0203703be8bdc18aa83bee0
rimersara <sara.p.rimer@gmail.com>
2025-07-17 14:27:41 -0400

    changes to step 2b python file

--- 02-network-edge-calibration/step2b_setup_ergm_fit_procedure.py
+++ 02-network-edge-calibration/step2b_setup_ergm_fit_procedure.py
@@ -18,6 +18,7 @@ random_seed_max = yamldata['max.random.seed']
 # setup args input filename 
 # expiriment_dir = f"{yamldata['repo.dir']}{yamldata['calibration.subdir']}{yamldata['expname']}/"
 expiriment_dir = f"{yamldata['repo.dir']}{yamldata['calibration.subdir']}"
+expiriment_interim_dir = f"{expiriment_dir}{yamldata['interim.data.subdir']}"
 # output_fname = f"{yamldata['step2b.inputargs.fname']}_{yamldata['expname']}.txt"
 output_fname = f"{yamldata['step2b.inputargs.fname']}.txt"
 
@@ -44,67 +45,67 @@ with open(output_file, 'w') as file:
 				file.write(line)
 				run += 1
 
-# sbatch = f"""
-# #SBATCH --account=p32153  ## YOUR ACCOUNT pXXXX or bXXXX
-# #SBATCH --partition={yamldata['sbatch.partition']}  ### PARTITION (buyin, short, normal, etc)
-# #SBATCH --array=0-{run-1} ## number of jobs to run "in parallel"
-# #SBATCH --nodes=1 ## how many computers do you need
-# #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-# #SBATCH --time={yamldata['sbatch.walltime.hours']}:{yamldata['sbatch.walltime.minutes']}:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-# #SBATCH --mem={yamldata['sbatch.memory']}
-# #SBATCH --job-name="{yamldata['expname']}_%A" ## When you run squeue -u NETID this is how you can identify the job
-# #SBATCH --output=%a_{run}.%A.{yamldata['expname']}.out ## standard out and standard error goes to this file
-# #SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (complet$
-# #SBATCH --mail-user={yamldata['sbatch.email']} ## your email
+sbatch = f"""
+#SBATCH --account=p32153  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition={yamldata['sbatch.partition']}  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --array=0-{run-1} ## number of jobs to run "in parallel"
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time={yamldata['sbatch.walltime.hours']}:{yamldata['sbatch.walltime.minutes']}:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem={yamldata['sbatch.memory']}
+#SBATCH --job-name="{yamldata['expname']}_%A" ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=%a_{run}.%A.{yamldata['expname']}.out ## standard out and standard error goes to this file
+#SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (complet$
+#SBATCH --mail-user={yamldata['sbatch.email']} ## your email
 
-# module purge all
-# conda activate /projects/p32153/condaenvs/conda-chistig
+module purge all
+conda activate /projects/p32153/condaenvs/conda-chistig
 
-# R --version
+R --version
 
-# IFS=$'\\n' read -d '' -r -a input_args < {output_fname}
-# echo ${{input_args[$SLURM_ARRAY_TASK_ID]}}
+IFS=$'\\n' read -d '' -r -a input_args < {output_fname}
+echo ${{input_args[$SLURM_ARRAY_TASK_ID]}}
 
-# IFS=$'\\t' read -r runno yamlfname calibrationset treatmenttype partnershiptype  <<< "${{input_args[$SLURM_ARRAY_TASK_ID]}}"
-# echo "Run number in SLURM array: ${{runno}}." 
-# echo "YAML file name: ${{yamlfname}}."
-# echo "Calibration set number for ERGM fit: ${{calibrationset}}." 
-# echo "Treatment type for ERGM fit: ${{treatmenttype}}."
-# echo "Partnership type for ERGM fit: ${{partnershiptype}}."
+IFS=$'\\t' read -r runno yamlfname calibrationset treatmenttype partnershiptype  <<< "${{input_args[$SLURM_ARRAY_TASK_ID]}}"
+echo "Run number in SLURM array: ${{runno}}." 
+echo "YAML file name: ${{yamlfname}}."
+echo "Calibration set number for ERGM fit: ${{calibrationset}}." 
+echo "Treatment type for ERGM fit: ${{treatmenttype}}."
+echo "Partnership type for ERGM fit: ${{partnershiptype}}."
 
-# my_random_seed_array={random_seeds_string}
-# max_attempts=${{#my_random_seed_array[@]}}
+my_random_seed_array={random_seeds_string}
+max_attempts=${{#my_random_seed_array[@]}}
 
-# echo "Array of random seeds for convergence attempts: ${{my_random_seed_array[@]}}."
+echo "Array of random seeds for convergence attempts: ${{my_random_seed_array[@]}}."
 
-# timeout_duration={yamldata['ergm.convergence.attempt.time']*60}
+timeout_duration={yamldata['ergm.convergence.attempt.time']*60}
 
-# for attempt in "${{!my_random_seed_array[@]}}"; do
+for attempt in "${{!my_random_seed_array[@]}}"; do
 
-# 	random_seed=${{my_random_seed_array[attempt]}}
-# 	echo "Random seed is $random_seed for attempt $((attempt+1)) of $max_attempts..."
+	random_seed=${{my_random_seed_array[attempt]}}
+	echo "Random seed is $random_seed for attempt $((attempt+1)) of $max_attempts..."
 
-# 	# if timeout "$timeout_duration" Rscript {yamldata['step2b.rscript.fname']} "$yamlfname" "$calibrationset" "$treatmenttype" "$partnershiptype" "${{my_random_seed_array[$attempt]}}"; then
-# 	if timeout "$timeout_duration" Rscript step2b_edge_calibration.R "$yamlfname" "$calibrationset" "$treatmenttype" "$partnershiptype" "${{my_random_seed_array[$attempt]}}"; then
-#         echo "Attempt $((attempt+1)) completed successfully."
-#         break
-#     else
-#     	echo "Attempt $((attempt+1)) did not finish within the timeout period."
-#     fi
+	# if timeout "$timeout_duration" Rscript {yamldata['step2b.rscript.fname']} "$yamlfname" "$calibrationset" "$treatmenttype" "$partnershiptype" "${{my_random_seed_array[$attempt]}}"; then
+	if timeout "$timeout_duration" Rscript step2b_edge_calibration.R "$yamlfname" "$calibrationset" "$treatmenttype" "$partnershiptype" "${{my_random_seed_array[$attempt]}}"; then
+        echo "Attempt $((attempt+1)) completed successfully."
+        break
+    else
+    	echo "Attempt $((attempt+1)) did not finish within the timeout period."
+    fi
 
-#     # If this is the last attempt, exit the loop
-#     if [ $((attempt+1)) -eq $max_attempts ]; then
-#         echo "Reached the maximum number of attempts ($max_attempts)."
-#     fi
+    # If this is the last attempt, exit the loop
+    if [ $((attempt+1)) -eq $max_attempts ]; then
+        echo "Reached the maximum number of attempts ($max_attempts)."
+    fi
 
-# done
+done
 
-# echo "Finished convergence attempts loop."
-# """
+echo "Finished convergence attempts loop."
+"""
 
-# outfile_temp = f'{expiriment_dir}temp.sh'
-# # outfile_temp = f'temp.sh'
-# with open(outfile_temp, 'w') as f:
-# 	f.write(sbatch)
-# open(f'{sbatch_bash_commands_outfile}', "w").write("#!/bin/bash\n" + open(outfile_temp).read())
+outfile_temp = f'{expiriment_interim_dir}temp.sh'
+# outfile_temp = f'temp.sh'
+with open(outfile_temp, 'w') as f:
+	f.write(sbatch)
+open(f'{sbatch_bash_commands_outfile}', "w").write("#!/bin/bash\n" + open(outfile_temp).read())
 
