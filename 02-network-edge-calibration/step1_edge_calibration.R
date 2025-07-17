c87175071d36a9b8b096bdd25b1b568d8abdedc1
rimersara <sara.p.rimer@gmail.com>
2025-07-17 13:38:54 -0400

    edit to step1

--- 02-network-edge-calibration/step1_edge_calibration.R
+++ 02-network-edge-calibration/step1_edge_calibration.R
@@ -91,6 +91,7 @@ calibration_matrix <- expand.grid(experiment = expname,
      dplyr::select(fit_no, dplyr::everything())
 
 
+print(calibration_matrix)
 
 # # # Save `scenario_mat` as a CSV to be called on in next step
 # # ### Specify file name
