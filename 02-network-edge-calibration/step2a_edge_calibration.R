9f2785a112437230020919afa2ecb432b872f742
rimersara <sara.p.rimer@gmail.com>
2025-07-17 16:37:35 -0400

    testing step2 part a

--- 02-network-edge-calibration/step2a_edge_calibration.R
+++ 02-network-edge-calibration/step2a_edge_calibration.R
@@ -217,10 +217,15 @@ print(netstats_base$demog)
 # drate_mat <- drate_mat_full[num_calibration_scenarios, ]
 calibration_matrix <- calibration_matrix_full[num_calibration_scenarios, ]
 
-# print(calibration_matrix)
+print(calibration_matrix)
 
 print(netstats_base$main$fuzzynodematch_venues.all)
 print(calibration_matrix$venues_main)
+
+print(netstats_base$main$fuzzynodematch_apps.all)
+print(calibration_matrix$apps_main)
+
+
 # Netstats
 # for (i in 1:num_calibration_scenarios){
 # 	calibration_matrix <- calibration_matrix_full[i, ]
