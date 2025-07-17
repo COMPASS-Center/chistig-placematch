c1cef137c36e0c6588e9914a61afb97cc5b7b8a4
rimersara <sara.p.rimer@gmail.com>
2025-07-17 17:04:29 -0400

    cleaned up step2a

--- 02-network-edge-calibration/step2a_edge_calibration.R
+++ 02-network-edge-calibration/step2a_edge_calibration.R
@@ -11,39 +11,15 @@ args <- commandArgs(trailingOnly = TRUE)
 yamlfname <- args[1]
 yamldata <- yaml.load_file(yamlfname)
 
-
 ### Name datafiles 
-# egos_fname <- paste0(yamldata$repo.dir, yamldata$synthpop.subdir, yamldata$synthpop.fname)
-
-# epistats_fname <- paste0(yamldata$repo.dir, yamldata$prelim.subdir, yamldata$epistats.fname)
-# artnet_duration_dissolution_fname <- paste0(yamldata$repo.dir, yamldata$prelim.subdir, yamldata$artnet.duration.dissolution.fname)
-
 netstats_fname <- paste0(yamldata$repo.dir, yamldata$netstats.subdir, yamldata$netstats.fname)
-
-# targetstats_fname <- paste0(yamldata$repo.dir, yamldata$synthpop.subdir, yamldata$target.dataframe.fname)
-
-#drate_mat_fname <- paste0(yamldata$repo.dir, yamldata$calibration.matrix.subdir, yamldata$)
 calibration_matrix_fname <- paste0(yamldata$repo.dir, yamldata$calibration.subdir, yamldata$calibration.matrix.fname)
-
 
 if (is.null(yamldata$interim.data.subdir)) {
   outdir <- paste(yamldata$repo.dir, yamldata$calibration.subdir, sep="")
 } else {
   outdir <- paste(yamldata$repo.dir, yamldata$calibration.subdir, yamldata$interim.data.subdir, sep="")
 }
-
-### Read in epistats
-# readRDS("./data/intermediate/estimates/epistats-local.rds")
-# epistats <- readRDS(epistats_fname)
-# source(artnet_compute_duration_dissolution_fname)
-# dur_coefs <- readRDS(artnet_duration_dissolution_fname)
-
-
-### Read in the calibration input matrix 
-calibration_matrix_full <- read.csv(calibration_matrix_fname)
-num_calibration_scenarios <- max(calibration_matrix_full$fit_no)
-
-
 
 ####################################################
 # Obtain the base netstats object from prelim step #
@@ -55,6 +31,8 @@ netstats_base <- readRDS(netstats_fname)
 ##############################################################
 # Setup and compute netstats object for each calibration set #
 ##############################################################
+calibration_matrix_full <- read.csv(calibration_matrix_fname)
+num_calibration_scenarios <- max(calibration_matrix_full$fit_no)
 calibration_matrix <- calibration_matrix_full[num_calibration_scenarios, ]
 
 
