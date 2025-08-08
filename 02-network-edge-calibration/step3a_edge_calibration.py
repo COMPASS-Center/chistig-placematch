b1ed1787f1f6324797dd358abf8f33f6c520a0c5
rimersara <sara.p.rimer@gmail.com>
2025-08-08 16:01:52 -0400

    finished step 3a edits

--- 02-network-edge-calibration/step3a_edge_calibration.py
+++ 02-network-edge-calibration/step3a_edge_calibration.py
@@ -28,7 +28,7 @@ expiriment_interim_dir = f"{expiriment_dir}{yamldata['interim.data.subdir']}"
 
 # simulation dir 
 # simulation_dir = expiriment_interim_dir
-simulation_dir = expiriment_dir
+simulation_dir = expiriment_interim_dir
 
 # params dir
 params_dir = f"{yamldata['repo.dir']}{yamldata['params.subdir']}"
@@ -92,8 +92,8 @@ sbatch = f"""
 #SBATCH --array=0-{run-1} ## number of jobs to run "in parallel"
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
-#SBATCH --time={yamldata['sbatch.time.hours.sim']}:{yamldata['sbatch.time.minutes.sim']}:00 ## how long does this need to run (remember different partitions have restrictions on this param)
-#SBATCH --mem={yamldata['sbatch.mem.sim']}
+#SBATCH --time={yamldata['sbatch.walltime.hours.sim']}:{yamldata['sbatch.walltime.minutes.sim']}:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem={yamldata['sbatch.memory.sim']}
 #SBATCH --job-name="${{SLURM_ARRAY_TASK_ID}}_sim_{experiment_name}" ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --output=%a_{run}.%A.sim.{experiment_name}.out ## standard out and standard error goes to this file
 #SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (complet$
@@ -104,51 +104,50 @@ conda activate /projects/p32153/condaenvs/conda-chistig
 
 R --version
 
+IFS=$'\\n' read -d '' -r -a input_args < {yamldata['step3.simulation.args.fname']}
+echo ${{input_args[$SLURM_ARRAY_TASK_ID]}}
 
-# IFS=$'\\n' read -d '' -r -a input_args < {yamldata['simulation.arguments.file']}_{experiment_name}.txt
-# echo ${{input_args[$SLURM_ARRAY_TASK_ID]}}
+SECONDS=0
+Rscript step3a_simtest.R ${{input_args[$SLURM_ARRAY_TASK_ID]}}
+echo $SECONDS
+"""
 
-# SECONDS=0
-# Rscript simtest_{experiment_name}.R ${{input_args[$SLURM_ARRAY_TASK_ID]}}
-# echo $SECONDS
-# """
-
-# outfile_temp = f'{expiriment_dir}temp.sh'
-# # outfile_temp = f'temp.sh'
-# with open(outfile_temp, 'w') as f:
-#         f.write(sbatch)
-# open(f'{sbatch_simulation_bash_commands_outfile}', "w").write("#!/bin/bash\n" + open(outfile_temp).read())
-
-
-# # create the model params yaml file for the simtest
-# # cycle through the base model params yaml file
-# # if the yamldict key is in the overwrite in the calibration, then use the overwrite; otherwise use base
-# # write out yaml file to the params directory but with the experiment name 
-# default_sim_params_fname = f"{yamldata['sim.model.params.base.fname']}.yaml"
-# default_sim_params_file = f"{params_dir}{default_sim_params_fname}"
-# new_sim_params_file = f"{params_dir}{yamldata['sim.model.params.base.fname']}_{experiment_name}.yaml"
-
-# with open(default_sim_params_file) as stream:
-#     try:
-#         dafault_sim_yamldata = yaml.safe_load(stream)
-#     except yaml.YAMLError as exc:
-#         print(exc)
-
-# new_sim_yamldata = {}
-# for thiskey, thisvalue in dafault_sim_yamldata.items():
-#         if thiskey in yamldata['simulation.yaml.override']:
-#                 new_sim_yamldata[thiskey] = yamldata['simulation.yaml.override'][thiskey]
-#         else:
-#                 new_sim_yamldata[thiskey] = thisvalue
-
-# with open(new_sim_params_file, 'w') as outfile:
-#     yaml.dump(new_sim_yamldata, outfile, default_flow_style=False, sort_keys=False)
+outfile_temp = f'{expiriment_dir}temp.sh'
+# outfile_temp = f'temp.sh'
+with open(outfile_temp, 'w') as f:
+        f.write(sbatch)
+open(f'{sbatch_simulation_bash_commands_outfile}', "w").write("#!/bin/bash\n" + open(outfile_temp).read())
 
 
-# # copy the Rscript for the simtest and rename it to match this experiment
-# # move it to the `ChiSTIG_model/chistig/` subdirectory 
-# base_sim_file = f"{expiriment_dir}{yamldata['simulation.rscript.file']}_testXX.R"
-# testexp_sim_file = f"{simulation_dir}{yamldata['simulation.rscript.file']}_{experiment_name}.R"
-# shutil.copy(base_sim_file, testexp_sim_file)
+# create the model params yaml file for the simtest
+# cycle through the base model params yaml file
+# if the yamldict key is in the overwrite in the calibration, then use the overwrite; otherwise use base
+# write out yaml file to the params directory but with the experiment name 
+default_sim_params_fname = f"{yamldata['default.simulation.params.fname']}"
+default_sim_params_file = f"{params_dir}{default_sim_params_fname}"
+new_sim_params_file = f"{params_dir}{yamldata['simulation.params.fname']}"
+
+with open(default_sim_params_file) as stream:
+    try:
+        dafault_sim_yamldata = yaml.safe_load(stream)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+new_sim_yamldata = {}
+for thiskey, thisvalue in dafault_sim_yamldata.items():
+        if thiskey in yamldata['simulation.yaml.override']:
+                new_sim_yamldata[thiskey] = yamldata['simulation.yaml.override'][thiskey]
+        else:
+                new_sim_yamldata[thiskey] = thisvalue
+
+with open(new_sim_params_file, 'w') as outfile:
+    yaml.dump(new_sim_yamldata, outfile, default_flow_style=False, sort_keys=False)
+
+
+# copy the Rscript for the simtest and rename it to match this experiment
+# move it to the `ChiSTIG_model/chistig/` subdirectory 
+base_sim_file = f"{expiriment_dir}{yamldata['step3a.rscript.fname']}"
+testexp_sim_file = f"{simulation_dir}{yamldata['step3a.rscript.fname']}"
+shutil.copy(base_sim_file, testexp_sim_file)
 
 
