ea65b865ad845337af369224288e21f378273726
Tom Wolff <tom.wolff@northwestern.edu>
2025-08-27 16:15:43 -0500

    Another round of calibration

--- 03-epimodel-parameter-calibration/step1a_generate_automated_calibration.R
+++ 03-epimodel-parameter-calibration/step1a_generate_automated_calibration.R
@@ -412,10 +412,10 @@ calib_object <- list(
                           get_benchmark(var = "ir100.W") - get_benchmark(var = "exo.ir100.W")),
           params = paste0("hiv.trans.scale_", 1:4),
           initial_proposals = dplyr::tibble(
-            hiv.trans.scale_1 = sample(seq(13.27132, 13.27132, length.out = n_sims)), # Need to update for parameters
-            hiv.trans.scale_2 = sample(seq(3.225005, 3.225005, length.out = n_sims)),
-            hiv.trans.scale_3 = sample(seq(2.9784875, 2.9784875, length.out = n_sims)),
-            hiv.trans.scale_4 = sample(seq(1.0302112, 1.0302112, length.out = n_sims))
+            hiv.trans.scale_1 = sample(seq(12, 14, length.out = n_sims)), # Need to update for parameters
+            hiv.trans.scale_2 = sample(seq(3, 4, length.out = n_sims)),
+            hiv.trans.scale_3 = sample(seq(1.2, 3.2, length.out = n_sims)),
+            hiv.trans.scale_4 = sample(seq(.5, 1.5, length.out = n_sims))
           ),
           make_next_proposals =
             swfcalib::make_proposer_se_range(n_sims, retain_prop = 0.3),
