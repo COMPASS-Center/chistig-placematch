372e9533a13e6fffeb191a7760a8a8e155f73315
rimersara <sara.p.rimer@gmail.com>
2025-10-06 13:02:55 -0400

    quick fixes to 04 step

--- 04-chistig-model-simulation/04_model_simulation_setup.sh
+++ 04-chistig-model-simulation/04_model_simulation_setup.sh
@@ -7,5 +7,11 @@ R --version
 
 yaml_file=$1
 
+# set up the sbatch script and the input arguments
 python 04_setup_model_simulation_batch_runs.py "$yaml_file"
 
+# make the sbatch file executable
+chmod +x 04_sbatch.sh
+
+# submit the sbatch script to quest
+sbatch 04_sbatch.sh
