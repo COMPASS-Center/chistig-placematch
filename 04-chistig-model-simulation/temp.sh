3fb7f2221b3a1ce8c0093a2c8bbf75f20bf37f50
rimersara <sara.p.rimer@gmail.com>
2025-10-06 13:03:05 -0500

    changes to step4 when submitting to sbatch

--- 04-chistig-model-simulation/temp.sh
+++ 04-chistig-model-simulation/temp.sh
@@ -6,8 +6,9 @@
 #SBATCH --ntasks-per-node=1 ## how many cpus or processors do you need on each computer
 #SBATCH --time=08:00:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem=3G
-#SBATCH --job-name="%A" ## When you run squeue -u NETID this is how you can identify the job
-#SBATCH --output=%a_2.%A.out ## standard out and standard error goes to this file
+#SBATCH --job-name=%A.step4 ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=/projects/p32153/chistig-placematch/04-chistig-model-simulation/interim/step4.%a_2.%A.out ## standard out and standard error goes to this file
+#SBATCH --error=/projects/p32153/chistig-placematch/04-chistig-model-simulation/interim/step4.%a_2.%A.err
 #SBATCH --mail-type=ALL ## you can receive e-mail alerts from SLURM when your job begins and when your job finishes (complet$
 #SBATCH --mail-user=tom.wolff@northwestern.edu ## your email
 
