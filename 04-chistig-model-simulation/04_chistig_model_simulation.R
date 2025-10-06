80e692c38035d77d54e2294b5fb1b10545fac8b5
rimersara <sara.p.rimer@gmail.com>
2025-10-06 13:02:38 -0500

    fix to agent log file name

--- 04-chistig-model-simulation/04_chistig_model_simulation.R
+++ 04-chistig-model-simulation/04_chistig_model_simulation.R
@@ -71,7 +71,7 @@ chistig_colocation_params <- python_chistig$create_params(chistig_colocation_par
 
 
 # rename the agent_log file with the specific experiment
-chistig_colocation_params$agent.log.file <- paste0(output_subdir, treatment_run, "_", experiment_name, ".txt")
+chistig_colocation_params$agent.log.file <- paste0(output_subdir, "agent-log_", treatment_run, "_", experiment_name, ".txt")
 
 # set the random seed in the colocation
 python_chistig$set_random_seed(random_seed)
