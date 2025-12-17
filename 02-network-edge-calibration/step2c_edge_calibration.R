81ad40d4bd357ed32c14ecbb4f90c2c1761d628b
rimersara <sara.p.rimer@gmail.com>
2025-12-17 13:16:35 -0500

    added a break if the number of calibration sets that didn't converge equals the total number of calibration sets

--- 02-network-edge-calibration/step2c_edge_calibration.R
+++ 02-network-edge-calibration/step2c_edge_calibration.R
@@ -27,7 +27,6 @@ print(interim_subdir)
 out_subdir <- interim_subdir
 cset2skip_fname <- paste0(out_subdir, yamldata$convergence.fail.fname, ".txt")
 
-print(cset2skip_fname)
 # # set up input data directories 
 # sim_input_data_dir <- paste0(yamldata$repo.dir, yamldata$netest.subdir) #nolint
 
@@ -76,7 +75,12 @@ for (cset in 1:num_calibration_scenarios){
 }
 
 cset2skip <- unique(cset2skip)
-print(paste0("Will skip the following calibration sets: ", cset2skip))
+
+if (length(cset2skip) == num_calibration_scenarios) {
+  stop("Error: None of the calibration sets fully converged. Rerun the `edge_calibration_procedure_ONE.sh`")
+} else {
+    print(paste0("Will skip the following calibration sets: ", cset2skip))
+}
 
 
 for (cset in 1:num_calibration_scenarios) {
