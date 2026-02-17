a6851c24d3fee61c904ab5f28df4bbf8a6c2958e
rimersara <sara.p.rimer@gmail.com>
2026-02-17 14:35:44 -0500

    debugged by removing the agent log method in the chistig_colocation_model_reticular model

--- 05-simulation-sweeps/05_chistig_model_simulation.R
+++ 05-simulation-sweeps/05_chistig_model_simulation.R
@@ -91,89 +91,87 @@ python_chistig$run(chistig_colocation_params)
 python_chistig$next_step()
 
 
-# # Settings ---------------------------------------------------------------------
-# source(paste0(utils_subdir, "utils-0_project_settings.R"))
-# source(paste0(utils_subdir, "utils-epi_trackers.R"))
-# source(paste0(utils_subdir, "utils-targets.R"))
-# #
-# # Network fit files
-# epistats <- readRDS(paste0(epistats_subdir, yamldata$epistats.fname))
-# netstats <- readRDS(paste0(network_fit_subdir, yamldata$netstats.fname, "_", sim_instance, ".rds"))
-# est <- readRDS(paste0(network_fit_subdir, yamldata$netest.venues.fname, "_", sim_instance, ".rds"))
+# Settings ---------------------------------------------------------------------
+source(paste0(utils_subdir, "utils-0_project_settings.R"))
+source(paste0(utils_subdir, "utils-epi_trackers.R"))
+source(paste0(utils_subdir, "utils-targets.R"))
+#
+# Network fit files
+epistats <- readRDS(paste0(epistats_subdir, yamldata$epistats.fname))
+netstats <- readRDS(paste0(network_fit_subdir, yamldata$netstats.fname, "_", sim_instance, ".rds"))
+est <- readRDS(paste0(network_fit_subdir, yamldata$netest.venues.fname, "_", sim_instance, ".rds"))
 
 
 
-# epistats$age.breaks <- c(16, 20, 30)
-# epistats$age.limits <- c(16, 30)
+epistats$age.breaks <- c(16, 20, 30)
+epistats$age.limits <- c(16, 30)
 
-# netstats$attr$age <- sample(16:29, length(netstats$attr$age), replace = TRUE)
-# netstats$attr$age <- netstats$attr$age + sample(1:1000, length(netstats$attr$age), replace = TRUE)/1000
-
-
-# epimodel_params_df <- readr::read_csv(paste0(disease_params_subdir, yamldata$epimodel.params.fname))
-# param <- EpiModel::param.net(
-#   data.frame.params = epimodel_params_df,
-#   netstats          = netstats,
-#   epistats          = epistats,
-#   prep.start        = Inf,
-#   riskh.start       = Inf
-# )
+netstats$attr$age <- sample(16:29, length(netstats$attr$age), replace = TRUE)
+netstats$attr$age <- netstats$attr$age + sample(1:1000, length(netstats$attr$age), replace = TRUE)/1000
 
 
-# print(param)
+epimodel_params_df <- readr::read_csv(paste0(disease_params_subdir, yamldata$epimodel.params.fname))
+param <- EpiModel::param.net(
+  data.frame.params = epimodel_params_df,
+  netstats          = netstats,
+  epistats          = epistats,
+  prep.start        = Inf,
+  riskh.start       = Inf
+)
 
 
-# # Initial conditions
-# init <- EpiModelHIV::init_msm()
-
-# # Control settings
-# control <- control_msm(
-#   nsteps = 52 * 70,
-#   nsims  = 1,
-#   ncores = 1,
-#   raw.output = TRUE,
-#   cumulative.edgelist = TRUE,
-#   .tracker.list       = calibration_trackers,
-
-#   initialize.FUN =              chiSTIGmodules::initialize_msm_chi,
-#   aging.FUN =                   chiSTIGmodules::aging_msm_chi,
-#   departure.FUN =               chiSTIGmodules::departure_msm_chi,
-#   arrival.FUN =                 chiSTIGmodules::arrival_msm_chi,
-#   venues.FUN =                  chiSTIGmodules:::venues_msm_chi,
-#   partident.FUN =               chiSTIGmodules::partident_msm_chi,
-#   hivtest.FUN =                 chiSTIGmodules::hivtest_msm_chi,
-#   hivtx.FUN =                   chiSTIGmodules::hivtx_msm_chi,
-#   hivprogress.FUN =             chiSTIGmodules::hivprogress_msm_chi,
-#   hivvl.FUN =                   chiSTIGmodules::hivvl_msm_chi,
-#   resim_nets.FUN =              chiSTIGmodules::simnet_msm_chi,
-#   acts.FUN =                    chiSTIGmodules::acts_msm_chi,
-#   condoms.FUN =                 chiSTIGmodules::condoms_msm_chi,
-#   position.FUN =                chiSTIGmodules::position_msm_chi,
-#   prep.FUN =                    chiSTIGmodules::prep_msm_chi,
-#   hivtrans.FUN =                chiSTIGmodules::hivtrans_msm_chi,
-#   exotrans.FUN =                chiSTIGmodules:::exotrans_msm_chi,
-#   stitrans.FUN =                chiSTIGmodules::stitrans_msm_chi,
-#   stirecov.FUN =                chiSTIGmodules::stirecov_msm_chi,
-#   stitx.FUN =                   chiSTIGmodules::stitx_msm_chi,
-#   prev.FUN =                    chiSTIGmodules::prevalence_msm_chi,
-#   cleanup.FUN =                 chiSTIGmodules::cleanup_msm_chi,
-
-#   module.order = c("aging.FUN", "departure.FUN", "arrival.FUN", "venues.FUN",
-#                    "partident.FUN", "hivtest.FUN", "hivtx.FUN", "hivprogress.FUN",
-#                    "hivvl.FUN", "resim_nets.FUN", "acts.FUN", "condoms.FUN",
-#                    "position.FUN", "prep.FUN", "hivtrans.FUN", "exotrans.FUN",
-#                    "stitrans.FUN", "stirecov.FUN", "stitx.FUN", "prev.FUN", "cleanup.FUN")
-# )
+print(param)
 
 
-# #
-# start_time <- Sys.time()
-# sim <- netsim(est, param, init, control)
-# end_time <- Sys.time()
+# Initial conditions
+init <- EpiModelHIV::init_msm()
 
-# sim$sim_date <- end_time
+# Control settings
+control <- control_msm(
+  nsteps = 52 * 70,
+  nsims  = 1,
+  ncores = 1,
+  raw.output = TRUE,
+  cumulative.edgelist = TRUE,
+  .tracker.list       = calibration_trackers,
 
-sim <- "test"
+  initialize.FUN =              chiSTIGmodules::initialize_msm_chi,
+  aging.FUN =                   chiSTIGmodules::aging_msm_chi,
+  departure.FUN =               chiSTIGmodules::departure_msm_chi,
+  arrival.FUN =                 chiSTIGmodules::arrival_msm_chi,
+  venues.FUN =                  chiSTIGmodules:::venues_msm_chi,
+  partident.FUN =               chiSTIGmodules::partident_msm_chi,
+  hivtest.FUN =                 chiSTIGmodules::hivtest_msm_chi,
+  hivtx.FUN =                   chiSTIGmodules::hivtx_msm_chi,
+  hivprogress.FUN =             chiSTIGmodules::hivprogress_msm_chi,
+  hivvl.FUN =                   chiSTIGmodules::hivvl_msm_chi,
+  resim_nets.FUN =              chiSTIGmodules::simnet_msm_chi,
+  acts.FUN =                    chiSTIGmodules::acts_msm_chi,
+  condoms.FUN =                 chiSTIGmodules::condoms_msm_chi,
+  position.FUN =                chiSTIGmodules::position_msm_chi,
+  prep.FUN =                    chiSTIGmodules::prep_msm_chi,
+  hivtrans.FUN =                chiSTIGmodules::hivtrans_msm_chi,
+  exotrans.FUN =                chiSTIGmodules:::exotrans_msm_chi,
+  stitrans.FUN =                chiSTIGmodules::stitrans_msm_chi,
+  stirecov.FUN =                chiSTIGmodules::stirecov_msm_chi,
+  stitx.FUN =                   chiSTIGmodules::stitx_msm_chi,
+  prev.FUN =                    chiSTIGmodules::prevalence_msm_chi,
+  cleanup.FUN =                 chiSTIGmodules::cleanup_msm_chi,
+
+  module.order = c("aging.FUN", "departure.FUN", "arrival.FUN", "venues.FUN",
+                   "partident.FUN", "hivtest.FUN", "hivtx.FUN", "hivprogress.FUN",
+                   "hivvl.FUN", "resim_nets.FUN", "acts.FUN", "condoms.FUN",
+                   "position.FUN", "prep.FUN", "hivtrans.FUN", "exotrans.FUN",
+                   "stitrans.FUN", "stirecov.FUN", "stitx.FUN", "prev.FUN", "cleanup.FUN")
+)
+
+
+#
+start_time <- Sys.time()
+sim <- netsim(est, param, init, control)
+end_time <- Sys.time()
+
+sim$sim_date <- end_time
 
 saveRDS(sim, "simout.rds")
 # saveRDS(sim, paste0(output_subdir, "simout-", treatment, "_run-no-", treatment_run_number, ".rds")) #TODO 
