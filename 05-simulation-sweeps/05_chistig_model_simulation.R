0a4208e872cb105c4c06418a6e41a406ab8014d7
rimersara <sara.p.rimer@gmail.com>
2026-02-17 14:11:11 -0500

    debugging

--- 05-simulation-sweeps/05_chistig_model_simulation.R
+++ 05-simulation-sweeps/05_chistig_model_simulation.R
@@ -71,21 +71,21 @@ chistig_colocation_params_fname <- paste0(abm_params_subdir, yamldata$colocation
 print(chistig_colocation_params_fname)
 chistig_colocation_params <- python_chistig$create_params(chistig_colocation_params_fname)
 
-print("")
-print(chistig_colocation_params$agent.log.file)
+# print("")
+# print(chistig_colocation_params$agent.log.file)
 
 # rename the agent_log file with the specific experiment
 # chistig_colocation_params$agent.log.file <- paste0(output_subdir, "agent-log_", treatment_run, "_", experiment_name, ".txt") #TODO
-chistig_colocation_params$agent.log.file <- "agent_log.txt"
+# chistig_colocation_params$agent.log.file <- "agent_log.txt"
 
-print("")
-print(chistig_colocation_params$agent.log.file)
+# print("")
+# print(chistig_colocation_params$agent.log.file)
 
-# # set the random seed in the colocation
-# python_chistig$set_random_seed(random_seed)
+# set the random seed in the colocation
+python_chistig$set_random_seed(random_seed)
 
-# # set up the model
-# python_chistig$run(chistig_colocation_params)
+# set up the model
+python_chistig$run(chistig_colocation_params)
 
 # # have agents attend their first sets of venues
 # python_chistig$next_step()
