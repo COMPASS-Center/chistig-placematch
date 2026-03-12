b9a55487c57873bc0506cb83ec3f96bb73f5797e
rimersara <sara.p.rimer@gmail.com>
2026-03-12 14:12:23 -0400

    added in print statements for debugging

--- 05-simulation-sweeps/post_sweep_processing1.R
+++ 05-simulation-sweeps/post_sweep_processing1.R
@@ -109,8 +109,11 @@ results_list <- mclapply(1:length(instance_dirs), function(z) {
   })
 }, mc.cores = num_workers)
 
+print("Done processing the simout.rds files...")
+
 sim_targets <- dplyr::bind_rows(Filter(Negate(is.null), results_list))
 
+print("Done creating the sim_targets object...")
 
 mean_incid <-  sim_targets %>%
   # mutate(sim = treat) %>%
@@ -160,73 +163,21 @@ mean_incid <-  sim_targets %>%
   ) %>%
   ungroup()
 
+print("Done creating the mean_incid...")
+
 mean_incid2 <- mean_incid %>% dplyr::filter(time > 2900)
 
+print("Done creating the mean_incid2...")
+
 saveRDS(mean_incid2, "/projects/p32153/chistig-placematch/05-simulation-sweeps/data_processing/mean_incid_intermediary.rds")
+
+print("Done saving the mean_incid2 as an intermediary data object...")
 
 sim_targets_final <- sim_targets %>% dplyr::filter(time > (max(time)-520)) %>%
   dplyr::select(treat = siminstance, trial = replicate, time, dplyr::everything())
 
+print("Done creating the sim_targets_final object...")
+
 write.csv(sim_targets_final, "/projects/p32153/chistig-placematch/05-simulation-sweeps/data_processing/sim_targets.csv")
 
-
-
-# for (j in 1:nrow(mean_incid2)) {
-#
-#   if (j %% 1000 == 0) {
-#     print(j)
-#   }
-#
-#   this_row <- mean_incid2[j,]
-#   past_year <- mean_incid2 %>%
-#     filter(time <= this_row$time & time > (this_row$time-52)) %>%
-#     filter(siminstance == this_row$siminstance)
-#   sums <- as.data.frame(t(colSums(past_year[,3:ncol(past_year)])))
-#   sums$siminstance <- this_row$siminstance
-#   sums$replicate <- this_row$replicate
-#   sums$time <- this_row$time
-#
-#   if (j == 1) {
-#     annual_incid <- sums
-#   } else {
-#     annual_incid <- dplyr::bind_rows(annual_incid, sums)
-#   }
-# }
-
-for (j in 1:nrow(mean_incid2)) {
-
-  if (j %% 1000 == 0) {
-    print(j)
-  }
-
-  this_row <- mean_incid2[j,]
-  past_year <- mean_incid2 %>%
-    filter(time <= this_row$time & time > (this_row$time-52)) %>%
-    filter(siminstance == this_row$siminstance) %>%
-    filter(replicate == this_row$replicate)
-  means <- as.data.frame(t(colMeans(past_year[,3:ncol(past_year)])))
-  means$siminstance <- this_row$siminstance
-  means$time <- this_row$time
-  means$replicate <- this_row$replicate
-
-  if (j == 1) {
-    annual_incid2 <- means
-  } else {
-    annual_incid2 <- dplyr::bind_rows(annual_incid2, means)
-  }
-}
-
-annual_incid2$total.incid.rate.dispar.BW <- annual_incid2$total.incid.B - annual_incid2$total.incid.W
-annual_incid2$total.incid.rate.dispar.HW <- annual_incid2$total.incid.H - annual_incid2$total.incid.W
-
-annual_incid2$ir100.dispar.BW <- annual_incid2$ir100.B - annual_incid2$ir100.W
-annual_incid2$ir100.dispar.HW <- annual_incid2$ir100.H - annual_incid2$ir100.W
-annual_incid2$ir100.dispar.OW <- annual_incid2$ir100.O - annual_incid2$ir100.W
-
-
-annual_incid_final <- annual_incid2 %>% dplyr::filter(time > (max(time)-520)) %>%
-  dplyr::select(treat = siminstance, trial = replicate, time, dplyr::everything())
-
-
-write.csv(annual_incid_final, "/projects/p32153/chistig-placematch/05-simulation-sweeps/data_processing/annual_incid.csv")
-
+print("Done writing the sim_targets_final object...")
